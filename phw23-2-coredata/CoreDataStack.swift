86e6599ba3c742726007e9dfa50bb4e76dccee49
jasonhung <jasonhungckm5801@gmail.com>
2024-05-31 16:34:54 +0800

    v1

---
+++ phw23-2-coredata/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataStack.swift
+//  phw23-2-coredata
+//
+//  Created by jasonhung on 2024/5/31.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "phw23_2_coredata")
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    private init() {}
+}
--- phw23-2-coredata/ViewController.swift
+++ phw23-2-coredata/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stringLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateStringLabel()
     }
 
+    func updateStringLabel() {
+        if let values = fetchSimpleValues() {
+            stringLabel.text = values.joined(separator: "\n")
+        }
+    }
+    
+    func insertSimpleValue(_ value: String) {
+        let context = CoreDataStack.shared.context
+        let entity = NSEntityDescription.entity(forEntityName: "SimpleEntity", in: context)!
+        let simpleObject = NSManagedObject(entity: entity, insertInto: context)
+        
+        simpleObject.setValue(value, forKey: "simple_value")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Failed to save value: \(error)")
+        }
+    }
+    
+    func fetchSimpleValues() -> [String]? {
+        let context = CoreDataStack.shared.context
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "SimpleEntity")
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            return results.map { $0.value(forKey: "simple_value") as? String }.compactMap { $0 }
+        } catch {
+            print("Failed to fetch values: \(error)")
+            return nil
+        }
+    }
 
+    func deleteAllData() {
+        let context = CoreDataStack.shared.context
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "SimpleEntity")
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        
+        do {
+            try context.execute(batchDeleteRequest)
+            try context.save() // 保存上下文
+        } catch {
+            print("Failed to delete all data: \(error)")
+        }
+    }
+    
+
+    @IBAction func AddString(_ sender: Any) {
+        insertSimpleValue("Jason:\(Int.random(in: 0..<100))")
+        updateStringLabel()
+
+    }
+    
+    @IBAction func deleteAll(_ sender: Any) {
+        deleteAllData()
+        updateStringLabel()
+    }
+    
 }
 
